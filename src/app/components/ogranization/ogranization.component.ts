ccd3ce440dc7c57b47006bcc32608e97ca21b51e
Oksana Kulbaba <o.coolbaba@gmail.com>
2021-04-18 12:14:09 +0300

    Create application

---
+++ src/app/components/ogranization/ogranization.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ogranization',
+  templateUrl: './ogranization.component.html',
+  styleUrls: ['./ogranization.component.css']
+})
+export class OgranizationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
